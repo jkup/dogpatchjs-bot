5f7117eafb838c67ec9f0ffc3da57d5ddb1acbf4
Jon Kuperman <jon.kuperman@gmail.com>
2016-04-04 15:31:20 -0700

    basic DM listener

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 'use strict'
 
-const Twit = require('twit')
+// NPM
+const Twit   = require('twit')
 
+// Local Config
+const config = require('./local.json')
 
+// Twit Config
+const T      = new Twit(config)
+const stream = T.stream('user')
+
+stream.on('direct_message', function(directMsg) {
+  const message    = directMsg.direct_message.text
+  const messageId  = directMsg.direct_message.id_str
+  const screenName = directMsg.direct_message.sender.screen_name
+
+	console.log(message, screenName, messageId)
+})
